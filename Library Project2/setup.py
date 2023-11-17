63ea389f19512e4d9eccca35c4f398df44ef6241
Untara Eka Saputra <129562759+codewithun@users.noreply.github.com>
2023-11-17 15:34:00 +0700

    Add files via upload

---
+++ Library Project2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='simplecalculator',
+    version='0.1',
+    packages=['simplecalculator'],
+    install_requires=[],
+    entry_points={},
+    url='https://github.com/codewithun/simplecalculator',
+    license='MIT',
+    author='Untara Saputra',
+    author_email='untara337@example.com',
+    description='Simplecalculator yang membuat pusing.',
+)
